5a72971862411d893c668ce8be93c0b9fd2603c4
KevDev0247 <donkevin1980s@gmail.com>
2021-01-22 14:09:15 -0500

    Add background

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-
 import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function App() {
   return (
-    <div>
+    <div className="min-vh-100" style={{ backgroundImage: `url(${require("./imgs/background_1.png")})` }}>
       <Navbar />
     </div>
   )
