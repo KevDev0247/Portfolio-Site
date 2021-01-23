41788063a93e9efe5229a28f30f525167fcaf546
KevDev0247 <donkevin1980s@gmail.com>
2021-01-23 15:42:41 -0500

    Fix navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
 import "bootstrap/dist/css/bootstrap.min.css"
+import Home from "./components/Home/Home"
 
 export default function App() {
   return (
-    <div className="min-vh-100" style={{ backgroundImage: `url(${require("./imgs/background_1.png")})` }}>
-      <Navbar />
+    <div>
+      <Helmet>
+        <title>Kevin Zhijun Wang</title>
+      </Helmet>
+
+      <div className="min-vh-100" style={{ backgroundImage: `url(${require("./imgs/background_1.png")})` }}>
+        <Navbar />
+        <Home />
+      </div>
     </div>
   )
 }
